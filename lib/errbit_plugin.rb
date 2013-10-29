517b04c3acfc6ea4601c68b52107fa23f6e5e870
Cyril Mougel <cyril.mougel@gmail.com>
2013-10-29 17:42:18 +0100

    Avoid not same filename than class name IssueTracker::None should be not configured?

--- lib/errbit_plugin.rb
+++ lib/errbit_plugin.rb
@@ -1,5 +1,5 @@
 require "errbit_plugin/version"
 require "errbit_plugin/register_plugin"
-require "errbit_plugin/errbit_plugin"
+require "errbit_plugin/issue_tracker"
 require "errbit_plugin/validate_issue_tracker"
 require "errbit_plugin/issue_trackers/none"
--- lib/errbit_plugin/issue_trackers/none.rb
+++ lib/errbit_plugin/issue_trackers/none.rb
@@ -3,13 +3,17 @@ module ErrbitPlugin
     def label; 'none'; end
     def note; 'When no issue tracker has been configured, you will be able to leave comments on errors.'; end
     def fields; []; end
-    def configured?; true; end
+    ##
+    # The NoneIssueTracker is mark like configured? false because it not valid
+    # like a real IssueTracker
+    def configured?; false; end
     def check_params; true; end
     def create_issue; true; end
     def url; ''; end
     def comments_allowed?; true; end
   end
 end
+
 ErrbitPlugin::Register.add_issue_tracker(
   'IssueTracker::None',
   ErrbitPlugin::NoneIssueTracker
