a4bc19730ce9adf07a6a63e59b9f728df10f2945
Stephen Crosby <stevecrozz@gmail.com>
2014-09-05 17:59:22 -0700

    add pretty print for hashes in issues

--- lib/errbit_plugin/issue_tracker.rb
+++ lib/errbit_plugin/issue_tracker.rb
@@ -6,5 +6,22 @@ module ErrbitPlugin
       @app = app
       @params = params
     end
+
+    def pretty_hash(hash, nesting = 0)
+      tab_size = 2
+      nesting += 1
+
+      pretty  = "{"
+      sorted_keys = hash.keys.sort
+      sorted_keys.each do |key|
+        val = hash[key].is_a?(Hash) ? pretty_hash(hash[key], nesting) : hash[key].inspect
+        pretty += "\n#{' '*nesting*tab_size}"
+        pretty += "#{key.inspect} => #{val}"
+        pretty += "," unless key == sorted_keys.last
+
+      end
+      nesting -= 1
+      pretty += "\n#{' '*nesting*tab_size}}"
+    end
   end
 end
