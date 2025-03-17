071d6eb16c5193f7922a13e32ada0bd26463522a
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-17 11:56:41 +0100

    Rubocop

--- spec/errbit_plugin/validate_issue_tracker_spec.rb
+++ spec/errbit_plugin/validate_issue_tracker_spec.rb
@@ -287,14 +287,37 @@ describe ErrbitPlugin::ValidateIssueTracker do
 
     context "without errors method" do
       klass = Class.new(ErrbitPlugin::IssueTracker) do
-        def self.label; "foo"; end
-        def self.note; "foo"; end
-        def self.fields; ["foo"]; end
-        def self.icons; {}; end
-        def configured?; true; end
-        def create_issue; "http"; end
-        def close_issue; "http"; end
-        def url; "http"; end
+        def self.label
+          "foo"
+        end
+
+        def self.note
+          "foo"
+        end
+
+        def self.fields
+          ["foo"]
+        end
+
+        def self.icons
+          {}
+        end
+
+        def configured?
+          true
+        end
+
+        def create_issue
+          "http"
+        end
+
+        def close_issue
+          "http"
+        end
+
+        def url
+          "http"
+        end
       end
 
       it "not valid" do
