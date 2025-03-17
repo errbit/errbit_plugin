502baefa8ce7b9cf51660026a605e35cd1a524fc
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-17 14:33:11 +0100

    Refactoring

--- spec/errbit_plugin/registry_spec.rb
+++ spec/errbit_plugin/registry_spec.rb
@@ -3,56 +3,76 @@
 require "spec_helper"
 
 RSpec.describe ErrbitPlugin::Registry do
-  before do
-    ErrbitPlugin::Registry.clear_issue_trackers
-  end
+  before { ErrbitPlugin::Registry.clear_issue_trackers }
 
-  let(:tracker) {
-    tracker = Class.new(ErrbitPlugin::IssueTracker) do
+  let(:tracker) do
+    Class.new(ErrbitPlugin::IssueTracker) do
       def self.label
         "something"
       end
     end
-    tracker
-  }
+  end
 
   describe ".add_issue_tracker" do
-    context "with issue_tracker class valid" do
-      before do
-        allow(ErrbitPlugin::ValidateIssueTracker)
-          .to receive(:new)
-          .with(tracker)
-          .and_return(double(valid?: true, message: ""))
-      end
-      it "add new issue_tracker plugin" do
-        ErrbitPlugin::Registry.add_issue_tracker(tracker)
-        expect(ErrbitPlugin::Registry.issue_trackers).to eq({
-          "something" => tracker
-        })
-      end
-      context "with already issue_tracker with this key" do
-        it "raise ErrbitPlugin::AlreadyRegisteredError" do
+    context "when issue tracker is valid" do
+      context "when issue tracker is not registered" do
+        let(:issue_tracker_validator) do
+          instance_double(ErrbitPlugin::IssueTrackerValidator,
+            valid?: true,
+            errors: [])
+        end
+
+        before do
+          expect(ErrbitPlugin::IssueTrackerValidator)
+            .to receive(:new)
+            .with(tracker)
+            .and_return(issue_tracker_validator)
+        end
+
+        it "add new issue_tracker plugin" do
           ErrbitPlugin::Registry.add_issue_tracker(tracker)
-          expect {
+
+          expect(ErrbitPlugin::Registry.issue_trackers).to eq({"something" => tracker})
+        end
+      end
+
+      context "when issue tracker is already registered" do
+        let(:issue_tracker_validator) do
+          instance_double(ErrbitPlugin::IssueTrackerValidator,
+            valid?: true,
+            errors: [])
+        end
+
+        before { ErrbitPlugin::Registry.add_issue_tracker(tracker) }
+
+        it "raise ErrbitPlugin::AlreadyRegisteredError" do
+          expect do
             ErrbitPlugin::Registry.add_issue_tracker(tracker)
-          }.to raise_error(ErrbitPlugin::AlreadyRegisteredError)
+          end.to raise_error(ErrbitPlugin::AlreadyRegisteredError)
         end
       end
     end
 
-    context "with an IssueTracker not valid" do
+    context "when issue tracker is not valid" do
+      let(:issue_tracker_validator) do
+        instance_double(ErrbitPlugin::IssueTrackerValidator,
+          valid?: false,
+          errors: [])
+      end
+
       it "raise an IncompatibilityError" do
-        allow(ErrbitPlugin::ValidateIssueTracker)
+        expect(ErrbitPlugin::IssueTrackerValidator)
           .to receive(:new)
           .with(tracker)
-          .and_return(double(valid?: false, message: "foo", errors: []))
-        expect {
+          .and_return(issue_tracker_validator)
+
+        expect do
           ErrbitPlugin::Registry.add_issue_tracker(tracker)
-        }.to raise_error(ErrbitPlugin::IncompatibilityError)
+        end.to raise_error(ErrbitPlugin::IncompatibilityError)
       end
 
       it "puts the errors in the exception message" do
-        allow(ErrbitPlugin::ValidateIssueTracker)
+        expect(ErrbitPlugin::IssueTrackerValidator)
           .to receive(:new)
           .with(tracker)
           .and_return(double(valid?: false, message: "foo", errors: ["one", "two"]))
@@ -63,6 +83,47 @@ RSpec.describe ErrbitPlugin::Registry do
           expect(e.message).to eq("one; two")
         end
       end
+
     end
   end
+
+  # before do
+  #   ErrbitPlugin::Registry.clear_issue_trackers
+  # end
+  #
+  # let(:tracker) {
+  #   tracker = Class.new(ErrbitPlugin::IssueTracker) do
+  #     def self.label
+  #       "something"
+  #     end
+  #   end
+  #   tracker
+  # }
+  #
+  # describe ".add_issue_tracker" do
+  #   context "with an IssueTracker not valid" do
+  #     it "raise an IncompatibilityError" do
+  #       allow(ErrbitPlugin::ValidateIssueTracker)
+  #         .to receive(:new)
+  #         .with(tracker)
+  #         .and_return(double(valid?: false, message: "foo", errors: []))
+  #       expect {
+  #         ErrbitPlugin::Registry.add_issue_tracker(tracker)
+  #       }.to raise_error(ErrbitPlugin::IncompatibilityError)
+  #     end
+  #
+  #     it "puts the errors in the exception message" do
+  #       allow(ErrbitPlugin::ValidateIssueTracker)
+  #         .to receive(:new)
+  #         .with(tracker)
+  #         .and_return(double(valid?: false, message: "foo", errors: ["one", "two"]))
+  #
+  #       begin
+  #         ErrbitPlugin::Registry.add_issue_tracker(tracker)
+  #       rescue ErrbitPlugin::IncompatibilityError => e
+  #         expect(e.message).to eq("one; two")
+  #       end
+  #     end
+  #   end
+  # end
 end
