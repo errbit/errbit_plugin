bc9be475787a0a8246953e12e137b3a05644183e
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-17 15:30:20 +0100

    More tests

--- spec/errbit_plugin/none_issue_tracker_spec.rb
+++ spec/errbit_plugin/none_issue_tracker_spec.rb
@@ -18,4 +18,26 @@ RSpec.describe ErrbitPlugin::NoneIssueTracker do
   it { expect(subject.create_issue).to eq(false) }
 
   it { expect(subject.close_issue).to eq(false) }
+
+  it { expect(described_class.label).to eq("none") }
+
+  it { expect(described_class.note).to eq("When no issue tracker has been configured, you will be able to leave comments on errors.") }
+
+  it { expect(described_class.fields).to eq({}) }
+
+  # def self.icons
+  #   @icons ||= {
+  #     create: ["image/png", read_static_file("none_create.png")],
+  #     goto: ["image/png", read_static_file("none_create.png")],
+  #     inactive: ["image/png", read_static_file("none_inactive.png")]
+  #   }
+  # end
+  #
+  # def self.read_static_file(file)
+  #   File.read(
+  #     File.expand_path(
+  #       File.join(File.dirname(__FILE__), "..", "..", "..", "static", file)
+  #     )
+  #   )
+  # end
 end
