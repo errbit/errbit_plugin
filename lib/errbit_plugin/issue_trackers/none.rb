a38ac18248c9fe06778b64db382c5880a00caadd
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-17 11:42:17 +0100

    Rubocop

--- lib/errbit_plugin/issue_trackers/none.rb
+++ lib/errbit_plugin/issue_trackers/none.rb
@@ -2,12 +2,18 @@
 
 module ErrbitPlugin
   class NoneIssueTracker < IssueTracker
-    def self.label; "none"; end
-    def self.note
-      "When no issue tracker has been configured, you will be able to " <<
-      "leave comments on errors."
+    def self.label
+      "none"
     end
-    def self.fields; {}; end
+
+    def self.note
+      "When no issue tracker has been configured, you will be able to leave comments on errors."
+    end
+
+    def self.fields
+      {}
+    end
+
     def self.icons
       @icons ||= {
         create: ["image/png", read_static_file("none_create.png")],
@@ -15,18 +21,37 @@ module ErrbitPlugin
         inactive: ["image/png", read_static_file("none_inactive.png")],
       }
     end
+
     def self.read_static_file(file)
-      File.read(File.expand_path(File.join(
-        File.dirname(__FILE__), "..", "..", "..", "static", file)))
+      File.read(
+        File.expand_path(
+          File.join(File.dirname(__FILE__), "..", "..", "..", "static", file)
+        )
+      )
     end
+
     ##
     # The NoneIssueTracker is mark like configured? false because it not valid
     # like a real IssueTracker
-    def configured?; false; end
-    def errors; {}; end
-    def url; ""; end
-    def create_issue(*); false; end
-    def close_issue(*); false; end
+    def configured?
+      false
+    end
+
+    def errors
+      {}
+    end
+
+    def url
+      ""
+    end
+
+    def create_issue(*)
+      false
+    end
+
+    def close_issue(*)
+      false
+    end
   end
 end
 
