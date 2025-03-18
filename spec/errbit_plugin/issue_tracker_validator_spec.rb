4221a9d4792e7e7685944731b82382adcaa48561
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-18 17:02:13 +0100

    Update tests

--- spec/errbit_plugin/issue_tracker_validator_spec.rb
+++ spec/errbit_plugin/issue_tracker_validator_spec.rb
@@ -103,51 +103,53 @@ RSpec.describe ErrbitPlugin::IssueTrackerValidator do
       end
     end
 
-    # context "without configured? method" do
-    #   klass = Class.new(ErrbitPlugin::IssueTracker) do
-    #     def self.label
-    #       "foo"
-    #     end
-    #
-    #     def self.note
-    #       "foo"
-    #     end
-    #
-    #     def self.fields
-    #       ["foo"]
-    #     end
-    #
-    #     def self.icons
-    #       {}
-    #     end
-    #
-    #     def errors
-    #       true
-    #     end
-    #
-    #     def create_issue
-    #       "http"
-    #     end
-    #
-    #     def close_issue
-    #       "http"
-    #     end
-    #
-    #     def url
-    #       "http"
-    #     end
-    #   end
-    #
-    #   it "not valid" do
-    #     expect(ErrbitPlugin::IssueTrackerValidator.new(klass).valid?).to be false
-    #   end
-    #
-    #   it "say not implement configured? method" do
-    #     is = ErrbitPlugin::IssueTrackerValidator.new(klass)
-    #     is.valid?
-    #     expect(is.errors).to eql [[:instance_method_missing, :configured?]]
-    #   end
-    # end
+    context "without #configured? method" do
+      klass = Class.new(ErrbitPlugin::IssueTracker) do
+        def self.label
+          "foo"
+        end
+
+        def self.note
+          "foo"
+        end
+
+        def self.fields
+          ["foo"]
+        end
+
+        def self.icons
+          {}
+        end
+
+        def errors
+          true
+        end
+
+        def create_issue
+          "http"
+        end
+
+        def close_issue
+          "http"
+        end
+
+        def url
+          "http"
+        end
+      end
+
+      it "is not valid" do
+        expect(ErrbitPlugin::IssueTrackerValidator.new(klass).valid?).to eq(false)
+      end
+
+      it "say not implement configured? method" do
+        validator = ErrbitPlugin::IssueTrackerValidator.new(klass)
+
+        validator.valid?
+
+        expect(validator.errors).to eq([[:instance_method_missing, :configured?]])
+      end
+    end
 
     # context "without errors method" do
     #   klass = Class.new(ErrbitPlugin::IssueTracker) do
