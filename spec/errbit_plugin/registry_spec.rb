d43f616b1eeb14070b0cd9787e2308db0c7b900c
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-10 20:49:38 +0200

    Standard

--- spec/errbit_plugin/registry_spec.rb
+++ spec/errbit_plugin/registry_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
 describe ErrbitPlugin::Registry do
   before do
@@ -10,7 +10,7 @@ describe ErrbitPlugin::Registry do
   let(:tracker) {
     tracker = Class.new(ErrbitPlugin::IssueTracker) do
       def self.label
-        'something'
+        "something"
       end
     end
     tracker
@@ -22,16 +22,16 @@ describe ErrbitPlugin::Registry do
         allow(ErrbitPlugin::ValidateIssueTracker)
           .to receive(:new)
           .with(tracker)
-          .and_return(double(:valid? => true, :message => ''))
+          .and_return(double(valid?: true, message: ""))
       end
-      it 'add new issue_tracker plugin' do
+      it "add new issue_tracker plugin" do
         ErrbitPlugin::Registry.add_issue_tracker(tracker)
         expect(ErrbitPlugin::Registry.issue_trackers).to eq({
-          'something' => tracker
+          "something" => tracker
         })
       end
       context "with already issue_tracker with this key" do
-        it 'raise ErrbitPlugin::AlreadyRegisteredError' do
+        it "raise ErrbitPlugin::AlreadyRegisteredError" do
           ErrbitPlugin::Registry.add_issue_tracker(tracker)
           expect {
             ErrbitPlugin::Registry.add_issue_tracker(tracker)
@@ -41,26 +41,26 @@ describe ErrbitPlugin::Registry do
     end
 
     context "with an IssueTracker not valid" do
-      it 'raise an IncompatibilityError' do
+      it "raise an IncompatibilityError" do
         allow(ErrbitPlugin::ValidateIssueTracker)
           .to receive(:new)
           .with(tracker)
-          .and_return(double(:valid? => false, :message => 'foo', :errors => []))
+          .and_return(double(valid?: false, message: "foo", errors: []))
         expect {
           ErrbitPlugin::Registry.add_issue_tracker(tracker)
         }.to raise_error(ErrbitPlugin::IncompatibilityError)
       end
 
-      it 'puts the errors in the exception message' do
+      it "puts the errors in the exception message" do
         allow(ErrbitPlugin::ValidateIssueTracker)
           .to receive(:new)
           .with(tracker)
-          .and_return(double(:valid? => false, :message => 'foo', :errors => ['one', 'two']))
+          .and_return(double(valid?: false, message: "foo", errors: ["one", "two"]))
 
         begin
           ErrbitPlugin::Registry.add_issue_tracker(tracker)
         rescue ErrbitPlugin::IncompatibilityError => e
-          expect(e.message).to eq('one; two')
+          expect(e.message).to eq("one; two")
         end
       end
     end
