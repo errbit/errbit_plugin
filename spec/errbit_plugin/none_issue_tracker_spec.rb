f2318dfaec1c891feb445a3104b5ca6d31da0e44
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-10 21:38:56 +0200

    Update tests

--- spec/errbit_plugin/none_issue_tracker_spec.rb
+++ spec/errbit_plugin/none_issue_tracker_spec.rb
@@ -3,17 +3,11 @@
 require "spec_helper"
 
 RSpec.describe ErrbitPlugin::NoneIssueTracker do
-  subject { described_class.new({}) }
+  let(:options) { {} }
 
-  it { expect(described_class.label).to eq("none") }
+  subject { described_class.new(options) }
 
-  it { expect(described_class.note).to start_with("When no issue tracker") }
-
-  it { expect(described_class.fields).to eq({}) }
-
-  it { expect(described_class.icons).not_to be_empty }
-
-  # TODO: .read_static_file
+  it { expect(subject).to be_an(ErrbitPlugin::IssueTracker) }
 
   it { expect(subject.configured?).to eq(false) }
 
@@ -24,4 +18,14 @@ RSpec.describe ErrbitPlugin::NoneIssueTracker do
   it { expect(subject.create_issue).to eq(false) }
 
   it { expect(subject.close_issue).to eq(false) }
+
+  it { expect(described_class.label).to eq("none") }
+
+  it { expect(described_class.note).to start_with("When no issue tracker") }
+
+  it { expect(described_class.fields).to eq({}) }
+
+  it { expect(described_class.icons).not_to be_empty }
+
+  # TODO: .read_static_file
 end
