8e15ae2c7c85619a18b1870335f39798c871df1e
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-10 21:30:16 +0200

    Add failing tests

--- spec/errbit_plugin/none_issue_tracker_spec.rb
+++ spec/errbit_plugin/none_issue_tracker_spec.rb
@@ -3,4 +3,25 @@
 require "spec_helper"
 
 RSpec.describe ErrbitPlugin::NoneIssueTracker do
+  subject { described_class.new({}) }
+
+  it { expect(described_class.label).to eq("none") }
+
+  it { expect(described_class.note).to start_with("When no issue tracker") }
+
+  it { expect(described_class.fields).to eq({}) }
+
+  it { expect(described_class.icons).not_to be_empty }
+
+  # TODO: .read_static_file
+
+  it { expect(subject.configured?).to eq(false) }
+
+  it { expect(subject.errors).to eq({}) }
+
+  it { expect(subject.url).to eq("") }
+
+  it { expect(subject.create_issue).to eq(false) }
+
+  it { expect(subject.close_issue).to eq(false) }
 end
